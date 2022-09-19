84bbb0865fee1e57fed915e9e0ddc5d6b9056de5
Erik <esoto20@csub.edu>
2022-09-18 21:25:33 -0700

    tailwind + starter files
    This is the initialization of the project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-full">
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
